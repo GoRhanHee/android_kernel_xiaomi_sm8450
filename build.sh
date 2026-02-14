0ed2e4fec023ae171af1410715d1bddc6545e45f
GoRhanHee <gorhanhee01@gmail.com>
2026-02-14 17:44:07 +0900

    Import build.sh system

---
+++ build.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Import KernelSU-Next
+curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/dev/kernel/setup.sh" | bash -
+
+# Define toolchain variables
+CLANG_DIR=$PWD/toolchain/clang-r574158
+PATH=$CLANG_DIR/bin:$PATH
+
+# Check if toolchain exists
+if [ ! -f "$CLANG_DIR/bin/clang-21" ]; then
+    echo "-----------------------------------------------"
+    echo "Toolchain not found! Downloading..."
+    echo "-----------------------------------------------"
+    rm -rf $CLANG_DIR
+    mkdir -p $CLANG_DIR
+    pushd $CLANG_DIR > /dev/null
+    curl -LJOk https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/mirror-goog-main-llvm-toolchain-source/clang-r574158.tar.gz
+    tar xf mirror-goog-main-llvm-toolchain-source-clang-r574158.tar.gz
+    rm mirror-goog-main-llvm-toolchain-source-clang-r574158.tar.gz
+    echo "Cleaning up..."
+    popd > /dev/null
+fi
+
+# Setting 
+export ANDROID_BUILD_TOP=$(pwd)
+export ARCH=arm64
+
+# Cooking Kernel Source
+mkdir out
+
+MAKE_ARGS="
+LLVM=1 \
+LLVM_IAS=1 \
+ARCH=arm64 \
+O=out
+"
+
+make ${MAKE_ARGS} -j24 gorhanhee_defconfig gorhanhee.config || exit 1
+make ${MAKE_ARGS} -j24 || exit 1
+
+# Cooking boot.img
+mkdir prebuilts/output
+chmod +x ${ANDROID_BUILD_TOP}/prebuilts/*
+cd ${ANDROID_BUILD_TOP}/prebuilts
+
+unzip -jo ${ANDROID_BUILD_TOP}/prebuilts/${DEVICE}/boot.zip boot.img -d ${ANDROID_BUILD_TOP}/prebuilts/
+./magiskboot unpack boot.img
+cp ${ANDROID_BUILD_TOP}/out/arch/arm64/boot/Image ${ANDROID_BUILD_TOP}/prebuilts/kernel
+./magiskboot repack boot.img
+cp ${ANDROID_BUILD_TOP}/prebuilts/new-boot.img ${ANDROID_BUILD_TOP}/prebuilts/output/boot.img

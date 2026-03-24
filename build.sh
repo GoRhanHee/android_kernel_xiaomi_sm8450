c1a14e04b9407c389e49f98dddad213cd89b4c7a
GoRhanHee <gorhanhee01@gmail.com>
2026-03-24 22:44:19 +0900

    Nuke SuSFS

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # Import KernelSU-Next
-curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/dev_susfs/kernel/setup.sh" | bash -s dev_susfs
-
-# Import SuSFS
-patch -p1 < prebuilts/susfs_a12_5.10.patch
+curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -
 
 # Define toolchain variables
 CLANG_DIR=$PWD/toolchain/clang-r536225

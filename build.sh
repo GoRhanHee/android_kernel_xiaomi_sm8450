707c35214da91e891651ad47cd26be9d6d24db27
GoRhanHee <gorhanhee01@gmail.com>
2026-04-11 23:08:07 +0900

    build: edit import KSUN code

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Import KernelSU-Next
-curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/dev_susfs/kernel/setup.sh" | bash -
+curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -
 
 # Define toolchain variables
 CLANG_DIR=$PWD/toolchain/clang-r536225
